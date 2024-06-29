4d1fb4b883d3fff55e0d6c8f40d9885f4588f460
ichirojwang <ichiwang27@gmail.com>
2024-06-29 11:59:35 -0700

    test: put map

--- src/main/java/com/cmpt276/asn2/controllers/RectangleController.java
+++ src/main/java/com/cmpt276/asn2/controllers/RectangleController.java
@@ -7,13 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cmpt276.asn2.models.Rectangle;
 import com.cmpt276.asn2.models.RectangleRepository;
 
 import jakarta.servlet.http.HttpServletResponse;
+
 
 
 
@@ -99,5 +103,10 @@ public class RectangleController {
         return "redirect:/rectangles/details?rid=" + rectId;
     }
     
+    @PutMapping("path/{id}")
+    public String putMethodName(@PathVariable String id, @RequestBody String entity) {
+
+        return entity;
+    }
 
 }
